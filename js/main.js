b8351a6095e5a8682ea7cda8764013108c273c7b
Jeremiah Okai <62171387+okaijeremiah@users.noreply.github.com>
2023-01-08 23:57:46 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /*global $, jQuery, alert*/
-$(document).ready(function() {
+$(document).ready(function () {
 
   'use strict';
 
@@ -10,11 +10,11 @@ $(document).ready(function() {
 
   $(document).on("scroll", onScroll);
 
-  $('a[href^="#"]').on('click', function(e) {
+  $('a[href^="#"]').on('click', function (e) {
     e.preventDefault();
     $(document).off("scroll");
 
-    $('a').each(function() {
+    $('a').each(function () {
       $(this).removeClass('active');
       if ($(window).width() < 768) {
         $('.nav-menu').slideUp();
@@ -24,12 +24,12 @@ $(document).ready(function() {
     $(this).addClass('active');
 
     var target = this.hash,
-        menu = target;
+      menu = target;
 
     target = $(target);
     $('html, body').stop().animate({
       'scrollTop': target.offset().top - 80
-    }, 500, 'swing', function() {
+    }, 500, 'swing', function () {
       window.location.hash = target.selector;
       $(document).on("scroll", onScroll);
     });
@@ -39,7 +39,7 @@ $(document).ready(function() {
   function onScroll(event) {
     if ($('.home').length) {
       var scrollPos = $(document).scrollTop();
-      $('nav ul li a').each(function() {
+      $('nav ul li a').each(function () {
         var currLink = $(this);
         var refElement = $(currLink.attr("href"));
       });
@@ -51,9 +51,9 @@ $(document).ready(function() {
   // ========================================================================= //
 
 
-  $(window).scroll(function() {
+  $(window).scroll(function () {
     var scroll = $(window).scrollTop();
-    if (scroll > 200 ) {
+    if (scroll > 200) {
       $("#main-nav, #main-nav-subpage").slideDown(700);
       $("#main-nav-subpage").removeClass('subpage-nav');
     } else {
@@ -67,7 +67,7 @@ $(document).ready(function() {
   //  // RESPONSIVE MENU
   // ========================================================================= //
 
-  $('.responsive').on('click', function(e) {
+  $('.responsive').on('click', function (e) {
     $('.nav-menu').slideToggle();
   });
 
@@ -77,11 +77,25 @@ $(document).ready(function() {
 
   var typed = $(".typed");
 
-  $(function() {
+  $(function () {
     typed.typed({
       strings: ["Okai Jeremiah.", "a Data Analyst.", "a BI Developer.", "a Designer.", "a Freelancer."],
       typeSpeed: 120,
       loop: true,
+    });
+  });
+
+  // ========================================================================= //
+  //  Typing Js
+  // ========================================================================= //
+
+  var typing = $(".typing");
+
+  $(function () {
+    typing.typed({
+      strings: ["Thanks for Reaching Out. <br> I'll Reply Shortly"],
+      typeSpeed: 50,
+      loop: false,
     });
   });
 
@@ -92,20 +106,30 @@ $(document).ready(function() {
 
 
   $('.services-carousel').owlCarousel({
-      autoplay: true,
-      loop: true,
-      margin: 20,
-      dots: true,
-      nav: false,
-      responsiveClass: true,
-      responsive: { 0: { items: 1 }, 768: { items: 2 }, 900: { items: 4 } }
-    });
+    autoplay: true,
+    loop: true,
+    margin: 20,
+    dots: true,
+    nav: false,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 2
+      },
+      900: {
+        items: 4
+      }
+    }
+  });
 
   // ========================================================================= //
   //  magnificPopup
   // ========================================================================= //
 
-  var magnifPopup = function() {
+  var magnifPopup = function () {
     $('.popup-img').magnificPopup({
       type: 'image',
       removalDelay: 300,
@@ -122,7 +146,7 @@ $(document).ready(function() {
         // The "opener" function should return the element from which popup will be zoomed in
         // and to which popup will be scaled down
         // By defailt it looks for an image tag:
-        opener: function(openerElement) {
+        opener: function (openerElement) {
           // openerElement is the element on which popup was initialized, in this case its <a> tag
           // you don't need to add "opener" option if this code matches your needs, it's defailt one.
           return openerElement.is('img') ? openerElement : openerElement.find('img');
@@ -140,18 +164,20 @@ $(document).ready(function() {
 // ========================================================================= //
 //  Porfolio isotope and filter
 // ========================================================================= //
-$(window).load(function(){
+$(window).load(function () {
 
   var portfolioIsotope = $('.portfolio-container').isotope({
     itemSelector: '.portfolio-thumbnail',
     layoutMode: 'fitRows'
   });
 
-  $('#portfolio-flters li').on( 'click', function() {
+  $('#portfolio-flters li').on('click', function () {
     $("#portfolio-flters li").removeClass('filter-active');
     $(this).addClass('filter-active');
 
-    portfolioIsotope.isotope({ filter: $(this).data('filter') });
+    portfolioIsotope.isotope({
+      filter: $(this).data('filter')
+    });
   });
 
 })
@@ -159,17 +185,45 @@ $(window).load(function(){
 // ========================================================================= //
 //  Porfolio isotope and filter
 // ========================================================================= //
-$(document).ready(function(){
-  $("#see-more").hover(function(){
-      $(this).css("background-color", "#f7f7f7");
-      $(this).css("color", "#b8a07e");
-      $(this).css("transform", "translateY(-2px)");
-      $(this).css("font-size", "18px");
-  }, function(){
-      $(this).css("background-color", "#b8a07e");
-      $(this).css("color", "#f7f7f7");
-      $(this).css("transform", "translateY(0px)");
-      $(this).css("font-size", "inherit");
-      $(this).css("font-weight", "500");
+$(document).ready(function () {
+  $("#see-more").hover(function () {
+    $(this).css("background-color", "#f7f7f7");
+    $(this).css("color", "#b8a07e");
+    $(this).css("transform", "translateY(-2px)");
+    $(this).css("font-size", "18px");
+  }, function () {
+    $(this).css("background-color", "#b8a07e");
+    $(this).css("color", "#f7f7f7");
+    $(this).css("transform", "translateY(0px)");
+    $(this).css("font-size", "inherit");
+    $(this).css("font-weight", "500");
+  });
+});
+
+
+//Sending of Mails//
+
+$(document).ready(function () {
+  $('.contactForm').submit(function (e) {
+    e.preventDefault();
+
+    var name = $('#name').val();
+    var email = $('#email').val();
+    var subject = $('#subject').val();
+    var message = $('#message').val();
+
+    $.ajax({
+      url: "send.php",
+      method: "POST",
+      data: {
+        name: name,
+        email: email,
+        subject: subject,
+        message: message
+      },
+      success: function (data) {
+        $('#sendmessage').html(data);
+      }
+    });
   });
 });
